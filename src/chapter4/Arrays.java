ca49d7e0086839db7975b04bca3c2f91f0108735
vivanovs <ivanovs223@gmail.com>
2015-02-04 14:26:33 +0000

    Initial commit

---
+++ src/chapter4/Arrays.java
@@ -0,0 +1,40 @@
+package chapter4;
+
+public class Arrays {
+	int[][] arr;
+	String name;
+	
+	public Arrays(String name, int i, int j){
+		this.name = name;
+		arr = new int[i][j];
+	}
+	
+	public void initialize(){
+		int start = 100;		
+		for (int i= 0; i<this.arr.length; i++){			
+			for( int j=0; j <this.arr[i].length; j++){
+				arr[i][j] = start+j+1;
+			}
+			start = start + 100;
+		}
+	}
+	
+	public String printOneArray(int[] array){
+		String result = "";
+		for (int i : array){
+			result = result + "[" + i + "]";
+		}
+		return result;
+		
+	}
+	
+	@Override
+	public String toString(){
+		String result = "";
+		for(int[] i : this.arr){
+			result = result + "[" + printOneArray(i) + "]\n";		
+		}
+		return result;
+	}
+
+}
---
+++ src/chapter4/RunArray.java
@@ -0,0 +1,12 @@
+package chapter4;
+
+public class RunArray {
+	public static void main(String[] args) {
+		Arrays myarr;
+		myarr = new Arrays("Vlad", 5, 5);
+		myarr.initialize();
+
+		System.out.println(myarr);
+	}
+
+}
