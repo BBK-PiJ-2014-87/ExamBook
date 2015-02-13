efcd5b96840507018dd99304dc76eb9eda79fb4d
vivanovs <ivanovs223@gmail.com>
2015-02-13 09:04:48 +0000

    Updated test cases

--- src/chapter7/TestRiverRafting.java
+++ src/chapter7/TestRiverRafting.java
@@ -4,9 +4,12 @@ public class TestRiverRafting {
 
 	public static void main(String[] args) {
 		RiverRafting riverRafting = new RiverRafting();
+		
+		System.out.println("*******1 Exception********");
 		try {
 			riverRafting.crossRapid(11);
-			riverRafting.rowRaft("nervous");
+			riverRafting.rowRaft("happy");
+			System.out.println("Enjoy River Rafting");
 		}
 		catch (FallInRiverException e1){
 			System.out.println("Get back into the raft");
@@ -17,7 +20,43 @@ public class TestRiverRafting {
 		finally{
 			System.out.println("Pay for the advanture");
 		}
-		System.out.println("After try-catch block!");
+		System.out.println("After the try-catch block!");
+		
+		System.out.println("*******NO Exception********");
+		
+		try {
+			riverRafting.crossRapid(7);
+			riverRafting.rowRaft("happy");
+			System.out.println("Enjoy River Rafting");
+		}
+		catch (FallInRiverException e1){
+			System.out.println("Get back into the raft");
+		}
+		catch (DropOarException e2){
+			System.out.println("Don't panic");
+		}
+		finally{
+			System.out.println("Pay for the advanture");
+		}
+		System.out.println("After the try-catch block!");
+		
+System.out.println("******* 1 exceptions ********");
+		
+		try {
+			riverRafting.crossRapid(7);
+			riverRafting.rowRaft("nervous");
+			System.out.println("Enjoy River Rafting");
+		}
+		catch (FallInRiverException e1){
+			System.out.println("Get back into the raft");
+		}
+		catch (DropOarException e2){
+			System.out.println("Don't panic");
+		}
+		finally{
+			System.out.println("Pay for the advanture");
+		}
+		System.out.println("After the try-catch block!");
 
 	}
 
